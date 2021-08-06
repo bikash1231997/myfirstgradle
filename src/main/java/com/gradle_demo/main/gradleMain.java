cc54603b6974d29fafc45bf2d6213b1ecd3e3ff3
Bikash <bikashmohapatra1997@gmail.com>
2021-08-06 16:29:24 +0530

    this is my first gradle project

---
+++ src/main/java/com/gradle_demo/main/gradleMain.java
@@ -0,0 +1,11 @@
+package com.gradle_demo.main;
+
+public class gradleMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is main class");
+
+	}
+
+}

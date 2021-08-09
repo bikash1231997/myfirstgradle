79aaee8e440a0e83565beb075e746a2238dce542
Bikash <bikashmohapatra1997@gmail.com>
2021-08-09 12:47:21 +0530

    commit

--- src/main/java/com/gradle_demo/main/gradleMain.java
+++ src/main/java/com/gradle_demo/main/gradleMain.java
@@ -4,7 +4,7 @@ public class gradleMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is main class for print");
+		System.out.println("this is main class for printdd");
 
 	}
 
